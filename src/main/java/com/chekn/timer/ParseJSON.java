5335405637b3a2443de1a86b6b4763cd3849245f
chekn <chekn_auto@163.com>
2016-03-25 15:18:01 +0800

    batch read one unit

--- src/main/java/com/chekn/timer/ParseJSON.java
+++ src/main/java/com/chekn/timer/ParseJSON.java
@@ -1,7 +1,9 @@
 package com.chekn.timer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
@@ -15,11 +17,34 @@ public class ParseJSON {
 	
 	public static void main(String[] args){
 		try {
-			new ParseJSON().read("1","f9CSNHfQUNPBcngXk99q3lp9ZE1ohPBY");
+			ParseJSON pj=new ParseJSON();
+			int i=0;
+			for(String cptid:pj.getCptId()){
+				pj.read(cptid,"QQWalc2TFzew5nT6ZuqQIbheEzYMCbFG");
+				System.out.println((++i)+"、CptID:"+cptid+":******************************************");
+				//break;
+			}
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public List<String> getCptId() throws IOException{
+		List<String> cptIds=new ArrayList<String>();
+		
+		String url="http://wide.ksbao.com:8001/api/chapterMenu/getCptID/ZC_FC/648485";
+		Document doc= Jsoup.connect(url).get();
+		String jsonStrData=doc.body().text();
+		JSONObject jsonObjData=JSONObject.parseObject(jsonStrData);
+		JSONArray jsonArrDataItems=jsonObjData.getJSONArray("data");
+		for(Object jsonObjDataItem_o:jsonArrDataItems.toArray()){
+			JSONObject jsonObjDataItem=(JSONObject) jsonObjDataItem_o;
+			cptIds.add ( jsonObjDataItem.getString("CptID") );
+		}
+		
+		return cptIds;
 	}
 	
 	public void read(String cptID,String guid) throws IOException{
@@ -34,7 +59,6 @@ public class ParseJSON {
 		String jsonStrData=doc.body().text();
 		JSONObject jsonObjData=JSONObject.parseObject(jsonStrData);
 		String jsonStrMess=jsonObjData.getJSONObject("data").getString("test");
-		//System.out.println(jsonStrMess);
 		
 		JSONObject jsonObjMess=JSONObject.parseObject(jsonStrMess);
 		JSONArray jsonArrStyleItems=jsonObjMess.getJSONArray("StyleItems");
@@ -42,10 +66,10 @@ public class ParseJSON {
 		int typeLevel=1;
 		for(Object jsonObjStyleItem_O:jsonArrStyleItems.toArray()){	
 			JSONObject jsonObjStyleItem=(JSONObject) jsonObjStyleItem_O;
-			//System.out.println(jsonObjStyleItem.get("Type"));
+			String itemType=jsonObjStyleItem.getString("Type");
+			String itemStyle=jsonObjStyleItem.getString("Style");
 			System.out.println(StringUtils.defaultString((String)jsonObjStyleItem.get("SubType")));
 			System.out.println(jsonObjStyleItem.get("Explain"));
-			//System.out.println(jsonObjStyleItem.get("Style"));
 
 			int itemLevel=1;
 			JSONArray jsonArrTestItems=jsonObjStyleItem.getJSONArray("TestItems");
@@ -54,48 +78,52 @@ public class ParseJSON {
 				//三种题解析流程不同
 				JSONObject jsonObjTestItem=(JSONObject) jsonObjTestItem_O;
 				
-				if(typeLevel==1){
-					System.out.println(typeLevel+"."+itemLevel+"、"+jsonObjTestItem.get("Title"));
-					this.readSelectedItems(jsonObjTestItem);
-					System.out.print("\n");
-					
-					System.out.println(jsonObjTestItem.get("Answer"));
-					System.out.println(jsonObjTestItem.get("Explain"));
-					System.out.print("\n\n");
-				
-				}else if(typeLevel==2){
-					System.out.println(typeLevel+"."+itemLevel+"、"+jsonObjTestItem.get("FrontTitle"));
-					
-					int nItemLevel=1;
-					JSONArray jsonArrA3TestItems=jsonObjTestItem.getJSONArray("A3TestItems");
-					for(Object jsonObjA3TestItem_O:jsonArrA3TestItems.toArray()){
-						JSONObject jsonObjA3TestItem=(JSONObject) jsonObjA3TestItem_O;
-						System.out.println(typeLevel+"."+itemLevel+"."+nItemLevel+"、"+jsonObjA3TestItem.get("Title"));
-						this.readSelectedItems(jsonObjA3TestItem);
-						System.out.print("\n");
-						
-						System.out.println(jsonObjA3TestItem.get("Answer"));
-						System.out.println(jsonObjA3TestItem.get("Explain"));
-						nItemLevel++;
-					}
-					System.out.println(jsonObjTestItem.get("Explain"));
-					
-				}else {
-					JSONArray jsonArrBTestItems=jsonObjTestItem.getJSONArray("BTestItems");
-					int nItemLevel=1;
-					for(Object jsonObjBTestItem_O:jsonArrBTestItems.toArray()){
-						JSONObject jsonObjBTestItem=(JSONObject) jsonObjBTestItem_O;
-						System.out.println(typeLevel+"."+itemLevel+"."+nItemLevel+"、"+jsonObjBTestItem.get("Title"));
-						this.readSelectedItems(jsonObjTestItem);
-						System.out.print("\n");
-						
-						System.out.println(jsonObjBTestItem.get("Answer"));
-						System.out.println(jsonObjBTestItem.get("Explain"));
-						nItemLevel++;
-					}
+				switch(itemType){
+					case "ATEST": {
+							System.out.println(typeLevel+"."+itemLevel+"、"+jsonObjTestItem.get("Title")+"("+itemStyle+")");
+							this.readSelectedItems(jsonObjTestItem);
+							System.out.print("\n");
+							
+							System.out.println(jsonObjTestItem.get("Answer"));
+							System.out.println(jsonObjTestItem.get("Explain"));
+							System.out.print("\n\n");
+						}
+						break;
+					case "A3TEST":{ 
+							System.out.println(typeLevel+"."+itemLevel+"、"+jsonObjTestItem.get("FrontTitle")+"("+itemStyle+")");
+							
+							int nItemLevel=1;
+							JSONArray jsonArrA3TestItems=jsonObjTestItem.getJSONArray("A3TestItems");
+							for(Object jsonObjA3TestItem_O:jsonArrA3TestItems.toArray()){
+								JSONObject jsonObjA3TestItem=(JSONObject) jsonObjA3TestItem_O;
+								System.out.println(typeLevel+"."+itemLevel+"."+nItemLevel+"、"+jsonObjA3TestItem.get("Title"));
+								this.readSelectedItems(jsonObjA3TestItem);
+								System.out.print("\n");
+								
+								System.out.println(jsonObjA3TestItem.get("Answer"));
+								System.out.println(jsonObjA3TestItem.get("Explain"));
+								nItemLevel++;
+							}
+							System.out.println(jsonObjTestItem.get("Explain"));
+						}
+						break;
+					case "BTEST":{
+							JSONArray jsonArrBTestItems=jsonObjTestItem.getJSONArray("BTestItems");
+							int nItemLevel=1;
+							for(Object jsonObjBTestItem_O:jsonArrBTestItems.toArray()){
+								JSONObject jsonObjBTestItem=(JSONObject) jsonObjBTestItem_O;
+								System.out.println(typeLevel+"."+itemLevel+"."+nItemLevel+"、"+jsonObjBTestItem.get("Title")+"("+itemStyle+")");
+								this.readSelectedItems(jsonObjTestItem);
+								System.out.print("\n");
+								
+								System.out.println(jsonObjBTestItem.get("Answer"));
+								System.out.println(jsonObjBTestItem.get("Explain"));
+								nItemLevel++;
+							}
+						}
+						break;
 					
 				}
-				
 				itemLevel++;
 			}
 			
@@ -106,6 +134,9 @@ public class ParseJSON {
 		//System.out.println(jsonObjMess);
 	}
 	
+	public void appendFile(){
+		
+	}
 	
 	public void readSelectedItems(JSONObject jsonObj){
 		JSONArray jsonArrSelectedItems=jsonObj.getJSONArray("SelectedItems");
